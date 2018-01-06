34201401eda2689a580a8acdbfa6f91c5d760f2e
root <root@Gomezz>
2018-01-06 13:56:55 +0100

    insert update select delete

--- Database.h
+++ Database.h
@@ -14,6 +14,8 @@ public:
 	bool findTable(string pTableName);
         string select(string command,string user,string usage);
         string deleteFromTable(string commmand, string user);
+        string update(string command, string user);
+        string insert(string command, string user);
         bool check(string user,Table* table);
         bool porovnaj(string clenzpola, string clenfixny, string typ,string operand);
 	string toString(string pTableName);
